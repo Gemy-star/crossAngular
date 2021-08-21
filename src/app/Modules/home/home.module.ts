4207fea492f1f183e37b2f15e403149fe72148a6
Gemy-star <m.yassen.93@gmail.com>
2021-08-21 22:06:01 +0200

    final

--- src/app/Modules/home/home.module.ts
+++ src/app/Modules/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ButtonModule} from 'primeng/button';
 
 import { HomeRoutingModule } from './home-routing.module';
 import {BackgroundComponent} from "../../Components/background/background.component";
@@ -11,7 +12,8 @@ import {BackgroundComponent} from "../../Components/background/background.compon
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ButtonModule,
   ]
 })
 export class HomeModule { }
